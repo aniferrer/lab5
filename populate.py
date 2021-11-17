c9d161c63d375a9c9c48d556e4930c1620b3a72d
aniferrer <94547958+aniferrer@users.noreply.github.com>
2021-11-17 23:03:02 +0800

    Create populate.py

---
+++ populate.py
@@ -0,0 +1,102 @@
+import user_ops as user
+import order_ops as order
+from decimal import Decimal
+import random
+import time
+
+if __name__ == '__main__':
+    user.create_user("aarchamb", "Adam Archambault", "aarchambault0@wikipedia.org")
+    aarchamb_home   = {
+        "street": "1716 Fulton Lane",
+        "state": "Csongrad",
+        "country": "Hungary"
+    } 
+    aarchamb_office = {
+        "street": "6 Manitowish Place",
+        "state": "Csongrad",
+        "country": "Hungary"
+    }
+    user.add_address("aarchamb", "home", aarchamb_home)
+    user.add_address("aarchamb", "office", aarchamb_office)
+    
+    user.create_user("tgrimes1", "Trent Grimes", "tgrimes1@hostgator.com")
+    tgrimes1_home   = {
+        "street": "98905 Declaration Parkway",
+        "country": "Honduras"
+    } 
+    user.add_address("tgrimes1", "home", aarchamb_home)
+    
+    shopping_cart1 = [
+        {
+            'product_name' : 'New Apple MacBook Pro',
+            'price'        : Decimal('2018.42'),
+            'quantity'     : 1
+        },
+        {
+            'product_name' : 'Nintendo Switch',
+            'price'        : Decimal('368.95'),
+            'quantity'     : 1
+        },
+        {
+            'product_name' : 'Seagate Portable 2TB External',
+            'price'        : Decimal('59.95'),
+            'quantity'     : 1
+        }
+        ]
+    
+    shopping_cart2 = [
+        {
+            'product_name' : 'New Apple MacBook Pro',
+            'price'        : Decimal('2018.42'),
+            'quantity'     : 1
+        },
+        {
+            'product_name' : 'Charmin Ultra Soft Cushiony Touch Toilet Paper',
+            'price'        : Decimal('0.49'),
+            'quantity'     : 10
+        },
+        {
+            'product_name' : 'The Legend of Zelda: Breath of the Wild',
+            'price'        : Decimal('74.94'),
+            'quantity'     : 1
+        }
+        ]
+        
+    shopping_cart3 = [
+        {
+            'product_name' : 'HyperX Fury 16GB',
+            'price'        : Decimal('39.99'),
+            'quantity'     : 2
+        },
+        {
+            'product_name' : 'Jenga Classic Game',
+            'price'        : Decimal('19.99'),
+            'quantity'     : 1
+        },
+        {
+            'product_name' : 'Tasha\'s Cauldron of Everything',
+            'price'        : Decimal('49.95'),
+            'quantity'     : 3
+        }
+        ]
+    
+def str_time_prop(start, end, time_format, prop):
+
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
+    
+date1 = random_date("1/1/2021 12:00 AM", "11/19/2021 11:59 PM", random.random())
+date2 = random_date("1/1/2021 12:00 AM", "11/19/2021 11:59 PM", random.random())
+date3 = random_date("1/1/2021 12:00 AM", "11/19/2021 11:59 PM", random.random())
+    
+order.checkout("aarchamb", "home", shopping_cart1, date1)
+order.checkout("tgrimes1", "home", shopping_cart2, date2)
+order.checkout("tgrimes1", "home", shopping_cart3, date3)
+

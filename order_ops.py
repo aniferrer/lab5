44b2e4c41fda25b89d899df654bbe5ae3bfe3e65
aniferrer <94547958+aniferrer@users.noreply.github.com>
2021-11-17 23:02:24 +0800

    Create order_ops.py

---
+++ order_ops.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import boto3
+from boto3.dynamodb.conditions import Key
+import hashlib
+import random
+from decimal import Decimal
+
+def add_item(order_id, product_name, quantity, price): 
+    # Generate item ID. In real life, there are better
+    # ways of doing this
+    item_id = hashlib.sha256(product_name.encode()).hexdigest()[:8]
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users-orders-items')
+    
+    item = {
+        'pk'           : '#ITEM#{0}'.format(item_id), 
+        'sk'           : '#ORDER#{0}'.format(order_id),
+        'product_name' : product_name,
+        'quantity'     : quantity,
+        'price'        : price,
+        'status'       : "Pending"    
+    }
+    table.put_item(Item=item)
+    print("Added {0} to order {1}".format(product_name, order_id))
+    
+def checkout(username, address, items, date): 
+    # Generate order ID. In real life, there are better
+    # ways of doing this
+    order_id = hashlib.sha256(str(random.random()).encode()).hexdigest()[:random.randrange(1, 20)]
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users-orders-items')
+    
+    item = {
+        'pk'      : '#USER#{0}'.format(username), 
+        'sk'      : '#ORDER#{0}'.format(order_id),
+        'address' : address,
+        'status'  : "Placed",
+        'date'    : date,
+    }
+    table.put_item(Item=item)
+    
+    for item in items:
+        add_item(order_id, 
+                 item['product_name'], 
+                 item['quantity'], 
+                 item['price']
+                 )
+
+def query_user_status(username, status):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('users-orders-items')
+    response = table.query(
+        IndexName='status-index',
+        KeyConditionExpression=Key('pk').eq('#USER#{0}'.format(username)) & 
+                               Key('status').eq(status)
+    )
+    return response['Items']
+    
+def query_user_date(username, date):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('users-orders-items')
+    response = table.query(
+        IndexName='date-index',
+        KeyConditionExpression=Key('pk').eq('#USER#{0}'.format(username)) & 
+                               Key('date').eq(date)
+    )
+    return response['Items']
+
+def query_order_items(order_id):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('users-orders-items')
+    response = table.query(
+        IndexName='inverted-index',
+        KeyConditionExpression=Key('sk').eq('#ORDER#{0}'.format(order_id)) & 
+                               Key('pk').begins_with('#ITEM#')
+    )
+    return response['Items']
+
+def query_status_orders(status):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('users-orders-items')
+    response = table.query(
+        IndexName='pending-index',
+        KeyConditionExpression=Key('status').eq(status) & 
+                               Key('pk').begins_with('#ITEM#')
+    )
+    return response['Items']
